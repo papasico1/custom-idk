6ca06af25529f7fecb1a90010749aad835ec3e1c
aliabyo <161343889+aliabyo@users.noreply.github.com>
2024-04-30 09:32:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -259,11 +259,7 @@ class Bot(BaseBot):
                self.stop_announce()
                return
                 
-         if message.startswith("❤️ all"):
-           if user.username.lower() in self.moderators:
-             roomUsers = (await self.highrise.get_room_users()).content
-             for roomUser, _ in roomUsers:
-                await self.highrise.react("heart", roomUser.id)
+         
          if message.startswith("!kick"):
             if user.username.lower() in self.moderators:
                 parts = message.split()
@@ -365,7 +361,11 @@ class Bot(BaseBot):
                   await self.highrise.tip_user(roomUser.id, "gold_bar_5")
               else: 
                 await  self.highrise.send_whisper(user.id, f"Only _CAP0_ can use tip!")
-
+         if message.startswith("❤️ all"):
+             if user.username.lower() in self.moderators:
+                 roomUsers = (await self.highrise.get_room_users()).content
+                 for roomUser, _ in roomUsers:
+                      await self.highrise.react("heart", roomUser.id)
          if message == "!tip1":
               if user.username.lower() in owners:
                 roomUsers = (await self.highrise.get_room_users()).content
